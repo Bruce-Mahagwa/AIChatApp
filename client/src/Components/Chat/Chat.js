f5bac3aa2f8812af01c24483ffb3dfaaf4eca619
Bruce-Mahagwa <96821698+Bruce-Mahagwa@users.noreply.github.com>
2024-05-15 09:53:12 +0300

    Update Chat.js

--- client/src/Components/Chat/Chat.js
+++ client/src/Components/Chat/Chat.js
@@ -1,28 +1,39 @@
 import "./Chat.css"
-import {useEffect, useState, useRef, useLayoutEffect} from "react"
+import {useEffect, useState} from "react"
 import AvatarTextBox from "./AvatarTextBox"
 import { useSelector, useDispatch } from "react-redux"
 import { fetchAllChats } from "../../ReduxStore/Slice"
 import Input from "./Input"
 import axios from "axios"
+import loadingImg from "./loading-7528_256.gif"
+import errorImg from "./error-8660_256.gif"
 const Chat = () => {  
-    const {chatResponse, userChatMessage, sessionChats} = useSelector((state) => state.chats)
+    const {chatResponse, sessionChats} = useSelector((state) => state.chats)
     const [fetchChat, setFetchChat] = useState(false)
     const dispatch = useDispatch()
     const {loading} = chatResponse;
+    const [localLoading, setLocalLoading] = useState(false);
+    const [error, setError] = useState(false);
+
     useEffect(() => {
         // collects all posts from database when page loads
         const getPosts = async ()  => {
             if (sessionChats.length === 0 || fetchChat) {
                 // fires when session chats is empty or when someone triggers fetchChat to become true
+                // fetches all chats in the database
                 try {
+                    setLocalLoading(true)
                     const posts = await axios.get("/posts/getposts");
                     await dispatch(fetchAllChats(posts.data.posts));
+                    setLocalLoading(false)
+                    // ensures that the last chat comes into focus after loading all the chat history
                     const element = document.getElementById("chatbox").lastElementChild.previousElementSibling
                     element.scrollIntoView()
                 }
                 catch(e) {
                     console.log(e)
+                    setLocalLoading(false)
+                    setError(true)
                 }
             }
         }
@@ -34,11 +45,21 @@ const Chat = () => {
     return ( 
         <div id = "chatbox">
             <>
+            {/* jsx to show during loading */}
+            {localLoading && <div id = "loading">
+                <p>Loading Chats. Please wait...</p>
+                <img src = {loadingImg} alt = "loading gif" />
+            </div>}
+            {error && <div id = "error">
+                <p>We have encountered an error. Please reload the page</p>
+                <img src = {errorImg} alt = "error gif" />    
+            </div>}
+            {/* jsx to show during loading */}
             {sessionChats?.map((chat) => {
                 // custom container for a chat element.
                 // has two classes depending on whether the chat is from the user or the api
                 return (
-                <AvatarTextBox 
+                 <AvatarTextBox 
                     key = {chat.description}
                     description = {chat.description}
                     postedAt = {chat.postedAt}
